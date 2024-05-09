2c6e93ae30a30bca6e9fb41757c2c681bf23d9bc
Ruddyard_Castro <rcastroc2@miumg.edu.gt>
2024-05-09 16:45:26 -0600

    Cometarios secciones .h y .cpp

--- Proyecto_UMG/include/secciones.h
+++ Proyecto_UMG/include/secciones.h
@@ -1,15 +1,19 @@
-//Creado por Rocio Guisell Lopez espinoza 9959-23-740
+// Creado por Rocio Guisell Lopez espinoza 9959-23-740
 
+// Comprueba si SECCIONES_H ya ha sido definido, si no, lo define
 #ifndef SECCIONES_H
 #define SECCIONES_H
 
+// Incluye las bibliotecas est�ndar de C++ necesarias para el programa
 #include<stdlib.h>
 #include<cstdlib>
 #include<conio.h>
 #include<iomanip>
 
+
 using namespace std;
 
+// Define la estructura 'secciones' con los campos 'codigo', 'nombre' y 'estatus'
 struct secciones
 {
     int codigo;
@@ -17,6 +21,7 @@ struct secciones
     char estatus[1];
 };
 
+// Define la clase 'SeccionesCrud' con los m�todos p�blicos 'IngresarSeccion', 'ModificarSeccion', 'BorrarSeccion', 'DesplegarSeccion' y 'CrudSeccion'
 class SeccionesCrud
 {
     public:
@@ -28,5 +33,5 @@ class SeccionesCrud
 
 };//fin de la clase aula
 
+// Fin de la comprobaci�n de la definici�n de SECCIONES_H
 #endif // SECCIONES_H
-

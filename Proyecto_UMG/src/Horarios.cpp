892e29635f7816b2c189ba1a7e139492b9a16bbd
Daniel Reyna <dreynal@miumg.edu.gt>
2024-05-17 21:35:24 -0600

    Comentarios

--- Proyecto_UMG/src/Horarios.cpp
+++ Proyecto_UMG/src/Horarios.cpp
@@ -1,4 +1,5 @@
-//Creado por Daniel Reyna Lopez 9959-23-5291
+//Creado por Daniel Alberto Reyna Lopez 9959-23-5291
+
 
 #include "Horarios.h"
 #include <iostream>
@@ -11,18 +12,20 @@
 
 using namespace std;
 
+// Estructura para almacenar los datos de los horarios
 struct horarios {
-char id [11];
-char Sede [11];
-char Horario [11];
+    char id[11];      // ID del horario, longitud máxima de 10 caracteres
+    char Sede[11];    // Nombre de la sede, longitud máxima de 10 caracteres
+    char Horario[11]; // Horario, longitud máxima de 10 caracteres
 };
 
+// Método para mostrar el menú principal del sistema de gestión de horarios
 void Horarios::menu() {
     int choice;
     char x;
 
     do {
-        system("cls");
+        system("cls"); // Limpia la pantalla (en sistemas Windows)
         cout << "\t\t\t-----------------------------------------"<<endl;
         cout << "\t\t\t|   SISTEMA DE GESTION HORARIOS -  6200  |"<<endl;
         cout << "\t\t\t-----------------------------------------"<<endl;
@@ -40,22 +43,22 @@ void Horarios::menu() {
         switch(choice) {
         case 1:
             do {
-                insertar();
+                insertar(); // Llama al método para agregar un nuevo horario
                 cout << "\n\t\t\t Agregar otro Horario(Y,N): ";
                 cin >> x;
-            } while(x == 'y' || x == 'Y');
+            } while(x == 'y' || x == 'Y'); // Repite mientras el usuario ingrese 'Y' o 'y'
             break;
         case 2:
-            desplegar();
+            desplegar(); // Llama al método para desplegar todos los horarios
             break;
         case 3:
-            modificar();
+            modificar(); // Llama al método para modificar un horario existente
             break;
         case 4:
-            borrar();
+            borrar(); // Llama al método para borrar un horario
             break;
         case 5:
-            return;
+            return; // Sale del menú
         default:
             cout << "\n\t\t\t Opcion invalida...Por favor prueba otra vez..";
             cin.get();
@@ -63,18 +66,18 @@ void Horarios::menu() {
     } while(choice != 5);
 }
 
+// Método para insertar un nuevo horario
 void Horarios::insertar() {
-    system("cls");
-    Horarios horarios;
-    ofstream file("Horarios.dat", ios::binary | ios::app);
-
+    system("cls"); // Limpia la pantalla
+    Horarios horarios; // Crea una instancia de la estructura horarios
+    ofstream file("Horarios.dat", ios::binary | ios::app); // Abre el archivo en modo binario y de adición
 
     if (!file) {
         cerr << "No se pudo abrir el archivo." << endl;
         return;
     }
 
-
+    // Solicita los datos del nuevo horario al usuario
     cout << "\n--------------------------------------------------------------------------------------------------------------------";
     cout << "\n-------------------------------------------------Agregar Horario-------------------------------------------------------" << endl;
     cout << "\t\t\tIngrese id del Horario: ";
@@ -84,19 +87,18 @@ void Horarios::insertar() {
     cin.getline(horario.nombre, sizeof(horario.nombre));
     cout << "\t\t\tIngrese Horario : ";
     cin.getline(horario.Horario, sizeof(horario.Horario));
-    file.write(reinterpret_cast<const char*>(&horario), sizeof(horario));
-    file.close();
-    string codigoPrograma="6200";
+    file.write(reinterpret_cast<const char*>(&horario), sizeof(horario)); // Escribe los datos en el archivo
+    file.close(); // Cierra el archivo
+    string codigoPrograma = "6200";
     Bitacora Auditoria;
     string user;
-    Auditoria.ingresoBitacora(user,codigoPrograma,"INS");
+    Auditoria.ingresoBitacora(user, codigoPrograma, "INS"); // Registra la acción en la bitácora
 }
 
-
+// Método para desplegar todos los horarios almacenados
 void Horarios::desplegar() {
-    system("cls");
-    ifstream file("Horarios.dat", ios::binary);
-
+    system("cls"); // Limpia la pantalla
+    ifstream file("Horarios.dat", ios::binary); // Abre el archivo en modo binario
 
     if (!file) {
         cerr << "No se pudo abrir el archivo." << endl;
@@ -105,10 +107,9 @@ void Horarios::desplegar() {
 
     int total = 0;
 
-
     cout << "\n-----------------------------------------Tabla Detalles de Horarios ----------------------------------------------" << endl;
     Horarios horarios;
-    while (file.read(reinterpret_cast<char*>(&horario), sizeof(horario))) {
+    while (file.read(reinterpret_cast<char*>(&horario), sizeof(horario))) { // Lee los datos del archivo
         cout << "\t\t\t ID del Horario    : " << horario.id << endl;
         cout << "\t\t\t Nombre de la Sede : " << horario.nombre << endl;
         cout << "\t\t\t Horario de la Sede: " << horario.Horario << endl;
@@ -120,20 +121,19 @@ void Horarios::desplegar() {
         cout << "\n\t\t\tNo hay informacion..." << endl;
     }
 
-    file.close();
+    file.close(); // Cierra el archivo
     cin.ignore();
-    system("pause");
-    string codigoPrograma="6200";
+    system("pause"); // Pausa la ejecución hasta que el usuario presione una tecla
+    string codigoPrograma = "6200";
     Bitacora Auditoria;
     string user;
-    Auditoria.ingresoBitacora(user,codigoPrograma,"CON");
+    Auditoria.ingresoBitacora(user, codigoPrograma, "CON"); // Registra la acción en la bitácora
 }
 
-
+// Método para modificar un horario existente
 void Horarios::modificar() {
-    system("cls");
-    fstream file("Horarios.dat", ios::in | ios::out | ios::binary);
-
+    system("cls"); // Limpia la pantalla
+    fstream file("Horarios.dat", ios::in | ios::out | ios::binary); // Abre el archivo en modo binario para lectura y escritura
 
     if (!file) {
         cout << "\n\t\t\tNo hay información...";
@@ -148,17 +148,16 @@ void Horarios::modificar() {
     cin >> id_modificar;
 
     Horarios horarios;
-    ofstream file1("temporal.dat", ios::binary);
-
+    ofstream file1("temporal.dat", ios::binary); // Abre un archivo temporal en modo binario
 
     if (!file1) {
         cout << "\n\t\t\tError al abrir el archivo temporal...";
         return;
     }
 
-
-    while (file.read(reinterpret_cast<char*>(&horario), sizeof(Horario))) {
+    while (file.read(reinterpret_cast<char*>(&horario), sizeof(Horario))) { // Lee los datos del archivo original
         if (strcmp(horario.id, id_modificar.c_str()) == 0) {
+            // Solicita los nuevos datos al usuario
             cout << "\t\t\tIngrese nuevo ID del Horario: ";
             cin >> horario.id;
             cout << "\t\t\tIngrese nuevo nombre de la Sede: ";
@@ -167,12 +166,12 @@ void Horarios::modificar() {
             cout << "\t\t\tIngrese el nuevo Horario: ";
             cin.getline(horario.Horario, sizeof(horario.Horario));
 
-            file1.write(reinterpret_cast<const char*>(&horario), sizeof(Horario));
+            file1.write(reinterpret_cast<const char*>(&horario), sizeof(Horario)); // Escribe los datos modificados en el archivo temporal
 
             cout << "\n\t\t\t Horario modificad correctamente!!!" << endl;
             found++;
         } else {
-            file1.write(reinterpret_cast<const char*>(&horario), sizeof(Horario));
+            file1.write(reinterpret_cast<const char*>(&horario), sizeof(Horario)); // Escribe los datos no modificados en el archivo temporal
         }
     }
 
@@ -180,36 +179,34 @@ void Horarios::modificar() {
         cout << "\n\t\t\tNo se encontro algun Horario con el ID proporcionado." << endl;
     }
 
-    file.close();
-    file1.close();
-    remove("Horarios.dat");
-    rename("temporal.dat", "Horarios.dat");
+    file.close(); // Cierra el archivo original
+    file1.close(); // Cierra el archivo temporal
+    remove("Horarios.dat"); // Elimina el archivo original
+    rename("temporal.dat", "Horarios.dat"); // Renombra el archivo temporal al nombre del archivo original
     cin.ignore();
-    system("pause");
-    string codigoPrograma="6200";
+    system("pause"); // Pausa la ejecución hasta que el usuario presione una tecla
+    string codigoPrograma = "6200";
     Bitacora Auditoria;
     string user;
-    Auditoria.ingresoBitacora(user,codigoPrograma,"ACT");
+    Auditoria.ingresoBitacora(user, codigoPrograma, "ACT"); // Registra la acción en la bitácora
 }
 
-
+// Método para borrar un horario
 void Horarios::borrar() {
-    system("cls");
+    system("cls"); // Limpia la pantalla
     string id_borrar;
     int found = 0;
 
     cout << "\n------------------------------------------Detalles Horario a Borrar-----------------------------------------------" << endl;
 
-    ifstream file("Horarios.dat", ios::binary);
-
+    ifstream file("Horarios.dat", ios::binary); // Abre el archivo en modo binario
 
     if (!file) {
         cout << "\n\t\t\tNo hay informacion...";
         return;
     }
 
-    ofstream file1("temporal.dat", ios::binary);
-
+    ofstream file1("temporal.dat", ios::binary); // Abre un archivo temporal en modo binario
 
     if (!file1) {
         cout << "\n\t\t\tError al abrir el archivo temporal...";
@@ -221,12 +218,13 @@ void Horarios::borrar() {
     cin >> id_borrar;
 
     Horario horario;
-    while (file.read(reinterpret_cast<char*>(&horario), sizeof(Horario))) {
+    while (file.read(reinterpret_cast<char*>(&horario), sizeof(Horario))) { // Lee los datos del archivo original
         if (strcmp(horario.id, id_borrar.c_str()) == 0) {
+            // Si se encuentra el ID, no se escribe en el archivo temporal
             cout << "\n\t\t\tBorrado de informacion exitoso!!!!" << "\n" << endl;
             found++;
         } else {
-            file1.write(reinterpret_cast<const char*>(&horario), sizeof(Horario));
+            file1.write(reinterpret_cast<const char*>(&horario), sizeof(Horario)); // Escribe los datos no borrados en el archivo temporal
         }
     }
 
@@ -234,16 +232,16 @@ void Horarios::borrar() {
         cout << "\n\t\t\tId de Horario no encontrado...." << "\n" << endl;
     }
 
-    file1.close();
-    file.close();
+    file1.close(); // Cierra el archivo temporal
+    file.close(); // Cierra el archivo original
 
-    remove("Horarios.dat");
-    rename("temporal.dat", "Horarios.dat");
+    remove("Horarios.dat"); // Elimina el archivo original
+    rename("temporal.dat", "Horarios.dat"); // Renombra el archivo temporal al nombre del archivo original
 
     cin.ignore();
-    system("pause");
-    string codigoPrograma="6200";
+    system("pause"); // Pausa la ejecución hasta que el usuario presione una tecla
+    string codigoPrograma = "6200";
     Bitacora Auditoria;
     string user;
-    Auditoria.ingresoBitacora(user,codigoPrograma,"DEL");
+    Auditoria.ingresoBitacora(user, codigoPrograma, "DEL"); // Registra la acción en la bitácora
 }

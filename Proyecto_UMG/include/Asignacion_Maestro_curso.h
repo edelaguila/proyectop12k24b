aa467df261e4d1a557c89d45604a6f31733a6594
Florencia Ariaga <mariagac@miumg.edu.gt>
2024-05-19 00:06:22 -0600

    Modificaciones al proceso de asignacion maestro a curso

--- Proyecto_UMG/include/Asignacion_Maestro_curso.h
+++ Proyecto_UMG/include/Asignacion_Maestro_curso.h
@@ -27,49 +27,13 @@ struct Asignacion{
 class Asignacion_Maestro_curso
 {
     public:
-        /*Asignacion_Maestro_curso( string codigo_maestro,string nombre_maestro,string apellido_maestro, string sede,string aula1,string seccion,string facultad,string carrera,string curso,string jornada,string horario);*/
         Asignacion_Maestro_curso();
-       /*string setCodigo(string codigo_maestro);
-        string getCodigo();
-
-        string setNombre(string nombre_maestro);
-       string getNombre();
-
-     string setApellido(string apellido_maestro);
-        string getApellido();
-
-        string setSede(string sede);
-        string getSede();
-
-        string setAula(string aula1);
-        string getAula();
-
-        string setSeccion(string seccion);
-       string getSeccion();
-
-        string setFacultad(string facultad);
-        string getFacultad();
-
-        string setCarrera(string carrera);
-    string getCarrera();
-
-
-        string setCurso(string curso);
-        string getCurso();
-
-        string setJornada(string jornada);
-        string getJorada();
-
-        string setHorario(string horario);
-    string getHorario();*/
-
 
     void Menu_catedratico();
     void asignarse_curso();
     void actas();
 
     private:
-         //string codigo_maestro,nombre_maestro,apellido_maestro,sede,aula,seccion,facultad,carrera,curso,jornada,horario;
 
 };
 
--- Proyecto_UMG/src/Asignacion_Maestro_curso.cpp
+++ Proyecto_UMG/src/Asignacion_Maestro_curso.cpp
@@ -16,26 +16,7 @@
 #include<conio.h>
 #include<iomanip>
 using namespace std;
-/*Asignacion_Maestro_curso:: Asignacion_Maestro_curso( string codigo_maestro,string nombre_maestro,string apellido_maestro, string sede,string aula,string seccion,string facultad,string carrera,string curso,string jornada,string horario)
-{
 
-
-    this -> codigo_maestro=codigo_maestro;
-    this -> nombre_maestro=nombre_maestro;
-    this-> apellido_maestro=apellido_maestro;
-    this-> sede=sede;
-    this-> aula=aula;
-    this-> seccion=seccion;
-    this-> facultad=facultad;
-    this-> carrera=carrera;
-    this-> curso=curso;
-    this -> jornada=jornada;
-    this-> horario=horario;
-}*/
-Asignacion_Maestro_curso:: Asignacion_Maestro_curso()
-{
-
-}
 
 void Asignacion_Maestro_curso::Menu_catedratico()
 {
@@ -98,19 +79,23 @@ void Asignacion_Maestro_curso::Menu_catedratico()
 }
 void Asignacion_Maestro_curso::asignarse_curso()
 {
+	system("cls");
+
 //Proceso asignación maestro a curso realizado por Kathia Contreras 9959-23-8246
 
 int num=1;
 Asignacion Asigna;
-cout<<"ingrese codigo maestro"<<endl;
+cout<<"ingrese codigo maestro: ";
 cin >> Asigna.codigo_maestro ;
 cin.ignore();
 
-cout <<"ingrese nombre catedratico"<<endl;
+cout <<"ingrese nombre catedratico: ";
 cin.getline(Asigna.nombre_maestro,50);
 
-cout <<"ingrese apellido catedratico"<<endl;
+cout <<"ingrese apellido catedratico: ";
 cin.getline(Asigna.apellido_maestro,50);
+
+system("cls");
 
 //REVISAR QUE NOMBRE COLOCAN A CLASE Y ARCHIVO SEDE AL GRUPO ASIGNADO
 
@@ -128,16 +113,17 @@ cin.getline(Asigna.apellido_maestro,50);
 
         cout << num<< ". " << sede1.nombre << endl;
 
-        cout << "------------------------------------------------------" << endl;
         num++;
     }
     archivo7.close(); // Cerrar el archivo
-    cout << "Presione Enter para ingresar la sede que desee...";
-    cin.ignore();
-    cin.get(); */
+    */
 
-cout <<"ingrese la sede"<<endl;
+cout << "------------------------------------------------" << endl;
+cout <<"ingrese la sede:";
 cin.getline(Asigna.sede,50);
+
+
+system("cls");
 
 /* **********   DESPLIEGUE AULAS.DAT   ********** */
 
@@ -157,18 +143,16 @@ cout<<"-----------------Aulas Existentes---------------------"<<endl;
 
         cout << num<< ". " << aula1.nombre << endl;
 
-        cout << "------------------------------------------------------" << endl;
         num++;
     }
 
     archivo.close(); // Cerrar el archivo
 
-    cout << "Presione Enter para poder ingresar el aula que desee...";
-    cin.ignore();
-    cin.get();
-
-cout <<"ingrese el aula"<<endl;
+cout << "------------------------------------------------" << endl;
+cout <<"ingrese el aula:";
 cin.getline(Asigna.aula,50);
+
+system("cls");
 
 /* **********   DESPLIEGUE SECCION.DAT   ********** */
     cout<<"-----------------Secciones Existentes---------------------"<<endl;
@@ -185,17 +169,17 @@ cin.getline(Asigna.aula,50);
     while (archivo1.read(reinterpret_cast<char*>(&seccion1), sizeof(secciones))) {
         // Imprime los datos de la seccion1 en la consola
         cout << num <<". " << seccion1.nombre << endl;
-        cout << "------------------------------------------------------" << endl;
         num++;
     }
     archivo1.close();
 
-    // Imprime un mensaje indicando al usuario que presione Enter para continuar
-    cout << "Presione Enter para poder ingresar la seccion que desee...";
-    cin.ignore();
-    cin.get();
-cout <<"ingrese la seccion"<<endl;
+cout << "------------------------------------------------" << endl;
+
+cout <<"ingrese la seccion: ";
 cin.getline(Asigna.seccion,50);
+
+
+system("cls");
 /* **********   DESPLIEGUE FACULTAD.DAT   ********** */
     num=1;
     cout << "-----------------Facultades Existentes---------------------" << endl;
@@ -209,15 +193,16 @@ cin.getline(Asigna.seccion,50);
     while (archivo2.read(reinterpret_cast<char*>(&facultad1), sizeof(Facultad))) {
 
         cout << num <<". "<<"Nombre: " << facultad1.nombre << endl;
-        cout << "------------------------------------------------------" << endl;
+        num++;
     }
 
     archivo2.close();
-    cout << "Presione Enter seleccionar la facultad que desee...";
-    cin.ignore();
-    cin.get();
-    cout <<"ingrese la facultad"<<endl;
+cout << "------------------------------------------------" << endl;
+    cout <<"ingrese la facultad: ";
     cin.getline(Asigna.facultad,50);
+
+
+system("cls");
 /* **********   DESPLIEGUE CARRERA.DAT   ********** */
 
     cout<<"-----------------Carreras Existentes---------------------"<<endl;
@@ -230,15 +215,16 @@ cin.getline(Asigna.seccion,50);
     num=1;
     while (archivo3.read(reinterpret_cast<char*>(&carrera1), sizeof(Carrera))) {
         cout << num <<". " << carrera1.nombre << endl;
-        cout << "------------------------------------------------------" << endl;
+        num++;
     }
     archivo3.close();
-    cout << "Presione Enter para poder ingresar la carrera que desee...";
-    cin.ignore();
-    cin.get();
+cout << "------------------------------------------------" << endl;
 
-    cout <<"ingrese la carrera"<<endl;
+    cout <<"ingrese la carrera: ";
     cin.getline(Asigna.carrera,50);
+
+
+system("cls");
 /* **********   DESPLIEGUE CURSO.DAT   ********** */
     cout<<"-----------------Cursos Existentes---------------------"<<endl;
    //abre el archivo en modo lectura binaria y lee los datos del archivo
@@ -253,17 +239,19 @@ cin.getline(Asigna.seccion,50);
     while (archivo4.read(reinterpret_cast<char*>(&Curso1), sizeof(Curso))) {
 		//Imprime los registros leidos del archivo
         cout << num <<". " << Curso1.nombre << endl;
-        cout << "------------------------------------------------------" << endl;
+        num++;
     }
 
     archivo4.close();
 
-    cout << "Presione Enter para poder ingresar el curso que desee asignar...";
-    cin.ignore();
-    cin.get();
+cout << "------------------------------------------------" << endl;
 
-    cout <<"ingrese el curso"<<endl;
+    cout <<"ingrese el curso: ";
     cin.getline(Asigna.curso,50);
+
+
+
+system("cls");
 /* **********   DESPLIEGUE JORNADAS.DAT   ********** */
 
     cout<<"-----------------Jornadas Existentes---------------------"<<endl;
@@ -280,15 +268,16 @@ cin.getline(Asigna.seccion,50);
     while (archivo5.read(reinterpret_cast<char*>(&Jornada1), sizeof(Jornada))) {
 			//Imprime los registros leidos del archivo
         cout << num <<". " << Jornada1.nombre << endl;
-        cout << "------------------------------------------------------" << endl;
+        num++;
     }
     archivo5.close();
-    cout << "Presione Enter para ingresar la jornada que desee asignar...";
-    cin.ignore();
-    cin.get();
-    cout <<"ingrese la jornada"<<endl;
+cout << "------------------------------------------------" << endl;
+
+    cout <<"ingrese la jornada: ";
     cin.getline(Asigna.jornada,50);
 
+
+system("cls");
 /* **********   DESPLIEGUE HORARIO.DAT   ********** */
    /* cout<<"-----------------Horarios Existentes---------------------"<<endl;
       //abre el archivo en modo lectura binaria y lee los datos del archivo
@@ -305,125 +294,31 @@ cin.getline(Asigna.seccion,50);
     while (archivo6.read(reinterpret_cast<char*>(&horario1), sizeof(Horario))) {
 			//Imprime los registros leidos del archivo
         cout << num <<". " << horario1.nombre << endl;
-        cout << "------------------------------------------------------" << endl;
+        num++;
     }
     archivo6.close();
-    cout << "Presione Enter para ingresar el horario que desee asignar...";
-    cin.ignore();
-    cin.get();*/
-    cout <<"ingrese el horario"<<endl;
+*/
+cout << "------------------------------------------------" << endl;
+
+    cout <<"ingrese el horario;";
     cin.getline(Asigna.horario,50);
+
+
+
+system("cls");
+cout << "------------------------------------------------" << endl;
+
     ofstream archivo8("asignaciones.dat", ios::binary | ios::app);
     archivo8.write(reinterpret_cast<const char*>(&Asigna), sizeof(Asignacion));
     archivo8.close();
     cout << "Asignacion ingresada exitosamente!" << endl;
+    cout << "------------------------------------------------" << endl;
+
 system("pause");
 
 
 }
-/*
-string Asignacion_Maestro_curso::setCodigo(string codigo_maestro)
+Asignacion_Maestro_curso:: Asignacion_Maestro_curso()
 {
-    this -> codigo_maestro=codigo_maestro;
-}
 
-string Asignacion_Maestro_curso::getCodigo()
-{
-    return this->codigo_maestro;
 }
-
-string Asignacion_Maestro_curso::setNombre(string nombre_maestro)
-{
-    this ->  nombre_maestro= nombre_maestro;
-}
-
-string Asignacion_Maestro_curso::getNombre()
-{
-    return this-> nombre_maestro;
-}
-
-string Asignacion_Maestro_curso::setApellido(string apellido_maestro)
-{
-    this ->  apellido_maestro= apellido_maestro;
-}
-
-string Asignacion_Maestro_curso::getApellido()
-{
-    return this-> apellido_maestro;
-}
-string Asignacion_Maestro_curso::setSede(string sede)
-{
-    this ->  sede= sede;
-}
-
-string Asignacion_Maestro_curso::getSede()
-{
-    return this-> sede;
-}
-string Asignacion_Maestro_curso::setAula(string aula)
-{
-    this ->  aula= aula;
-}
-
-string Asignacion_Maestro_curso::getAula()
-{
-    return this-> aula;
-}
-
-string Asignacion_Maestro_curso::setSeccion(string seccion)
-{
-    this ->  seccion= seccion;
-}
-
-string Asignacion_Maestro_curso::getSeccion()
-{
-    return this-> seccion;
-}
-
-string Asignacion_Maestro_curso::setFacultad(string facultad)
-{
-    this ->  facultad= facultad;
-}
-
-string Asignacion_Maestro_curso::getFacultad()
-{
-    return this-> facultad;
-}
-
-string Asignacion_Maestro_curso::setCarrera(string carrera)
-{
-    this ->  carrera= carrera;
-}
-
-string Asignacion_Maestro_curso::getCarrera()
-{
-    return this-> carrera;
-}
-string Asignacion_Maestro_curso::setCurso(string curso)
-{
-    this ->  curso= curso;
-}
-
-string Asignacion_Maestro_curso::getCurso()
-{
-    return this-> curso;
-}
-
-string Asignacion_Maestro_curso::setJornada(string jornada)
-{
-    this ->  jornada = jornada;
-}
-
-string Asignacion_Maestro_curso::getJorada()
-{
-    return this-> jornada;
-}
-string Asignacion_Maestro_curso::setHorario(string horario)
-{
-    this ->  horario= horario;
-}
-
-string Asignacion_Maestro_curso::getHorario()
-{
-    return this-> horario;
-}*/

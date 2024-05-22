59caa73846e48da2b56427e95b50cbfdb3ddecb7
Anderson Rodríguez <arodriguezp16@miumg.edu.gt>
2024-05-21 22:58:29 -0600

    Cambiando variables privadas a publicas

--- Proyecto_UMG/include/Sedes.h
+++ Proyecto_UMG/include/Sedes.h
@@ -8,10 +8,11 @@ using namespace std;
 class Sedes
 {
     private:
-        char id[10];
+
+	public:
+	    char id[10];
         char nombre[50];
         char telefono[20];
-	public:
 		void menu();
 		void insertar();
 		void desplegar();
